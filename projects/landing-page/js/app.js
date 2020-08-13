f2703fe5838c11f8cf57948ae6fb08a40d60716c
naiera-magdy <44544070+naiera-magdy@users.noreply.github.com>
2020-08-13 20:57:53 +0200

    Initial work

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -16,42 +16,92 @@
 /**
  * Define Global Variables
  * 
-*/
+ */
 
+const sections = document.querySelectorAll('section');
+const frag = document.createDocumentFragment();
+const nav = document.querySelector('nav');
+let navLinks = []; 
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
-*/
+ */
 
+// Add class 'active' to section when near top of viewport
+function ActiveElement() {
+  for (section of sections) {
+    const position = section.getBoundingClientRect();
+    if (position.top >= 0 && (position.bottom <= (window.innerHeight || document.documentElement.clientHeight))) {
+      section.classList.add('active');
+      console.log(navLinks);
+      for (navLink of navLinks) {
+        if(navLink.getAttribute('src') === '#'+section.id) {
+          navLink.classList.add('active__link');
+          break;
+        }
+      }
+    } else {
+      section.classList.remove('active');
+      for (navLink of navLinks) {
+        if(navLink.getAttribute('src') === '#'+section.id) {
+          navLink.classList.remove('active__link');
+          break;
+        }
+      }
+    }
+  }
+}
 
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.querySelector(".navbar__menu").style.backgroundColor = "#DDDDDD80";
+  } else {
+    document.querySelector(".navbar__menu").style.backgroundColor = "#00000000";
+  }
+  // Set sections as active
+  ActiveElement();
+}
+
+function buildMenu() {
+  for (const section of sections) {
+    if (section.style.display === 'none') {
+      continue;
+    }
+    const element = document.createElement('li');
+    element.innerHTML = section.getAttribute('data-nav');
+    element.setAttribute('src', '#' + section.id);
+    frag.appendChild(element);
+    navLinks.push(element);
+  }
+}
 
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
+ */
 
 
 // Scroll to anchor ID using scrollTO event
 
+// Build menu 
+buildMenu();
+
+// build the nav
+document.querySelector('#navbar__list').appendChild(frag);
 
 /**
  * End Main Functions
  * Begin Events
  * 
-*/
-
-// Build menu 
+ */
 
 // Scroll to section on link click
+nav.addEventListener('click', (event) => {
+  window.location.href = event.target.getAttribute('src');
+});
 
-// Set sections as active
-
-
+// Set as active section on scroll event
+document.addEventListener('scroll',scrollFunction);
